e9138e7742c8c500edc0e5f98647403e4e457ed3
SURAJ_KUMAR <surajk38656@gmail.com>
2024-09-07 12:29:50 +0530

    ReadLine

--- src/com/example/fileread/io/ReadFileData.java
+++ src/com/example/fileread/io/ReadFileData.java
@@ -3,7 +3,7 @@ import java.io.*;
 
 public class ReadFileData {
     // return a StringBuilder which contains file data
-    public StringBuffer readData(String fileName) throws FileNotFoundException, IOException {
+    public StringBuffer readData(String fileName) throws IOException {
         StringBuffer fileData = new StringBuffer();
         //reading the file data
         File file = new File(fileName);
--- src/com/example/fileread/main/MyMain.java
+++ src/com/example/fileread/main/MyMain.java
@@ -1,14 +1,18 @@
 package com.example.fileread.main;
 import com.example.fileread.io.ReadFileData;
+
 import java.io.IOException;
 public class MyMain {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         String pathname = "Resources/sample.txt";
         ReadFileData readFileData = new ReadFileData();
         try {
+            Thread.sleep(500);
             System.out.println(readFileData.readData(pathname));
         }catch (IOException e){
             System.err.println("IOException"+e.getMessage());
+        }catch (InterruptedException e){
+            System.err.println("InterruptedException");
         }
     }
 }
